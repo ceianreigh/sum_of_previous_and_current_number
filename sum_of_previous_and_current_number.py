b6a08b3450cec80b38adf3d3bb5fd1ec48cf1e94
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 20:58:43 +0800

    Add the instructions and pseudocode

---
+++ sum_of_previous_and_current_number.py
@@ -0,0 +1,15 @@
+# Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
+# pseudocode
+
+# create a variable called previous_number and set it to 0
+
+# create a for loop that iterates 10 times
+
+# create a variable called current_number and set it to i
+
+# create a variable called sum_of_previous_and_current_number and set it to previous_number + current_number
+
+# print sum_of_previous_and_current_number
+
+# update previous_number to current_number

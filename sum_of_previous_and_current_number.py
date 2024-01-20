5fa2d3d7cc8fbb53f57c3cb21ae435184222a151
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 21:12:58 +0800

    Add starting message and print current, previous, and sum of previous and current number

--- sum_of_previous_and_current_number.py
+++ sum_of_previous_and_current_number.py
@@ -2,6 +2,9 @@
 
 # pseudocode
 
+# add a starting message
+print("Printing current and previous number sum in a range(10)")
+
 # create a variable called previous_number and set it to 0
 previous_number = 0
 
@@ -13,8 +16,15 @@ for i in range(10):
     # create a variable called sum_of_previous_and_current_number and set it to previous_number + current_number
     sum_of_previous_and_current_number = previous_number + current_number
 
-    # print sum_of_previous_and_current_number
-    print(sum_of_previous_and_current_number)
+    # print the current, previous, and sum of previous and current number
+    print(
+        "The current number is",
+        current_number,
+        "while, the previous number is",
+        previous_number,
+        "and the sum of them is",
+        sum_of_previous_and_current_number,
+    )
 
     # update previous_number to current_number
     previous_number = current_number
